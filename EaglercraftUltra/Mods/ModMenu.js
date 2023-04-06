923235d85250764522935c03b610b4449d0d2df6
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2023-04-05 22:03:37 -0700

    Update ModMenu.js

--- EaglercraftUltra/Mods/ModMenu.js
+++ EaglercraftUltra/Mods/ModMenu.js
@@ -4,8 +4,8 @@
 
 
 document.addEventListener('keydown', function(event) {
-  if (event.ctrlKey && event.key === 'm') {
-    OpenModMenu();
+  if (event.code === 'KeyG') {
+  OpenModMenu();
   }
 });
 
