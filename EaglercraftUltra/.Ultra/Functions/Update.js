160e28a1f281cd5670b2864f21c90cfe7c7e2750
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2023-04-09 15:40:24 -0700

    Update Update.js

--- EaglercraftUltra/.Ultra/Functions/Update.js
+++ EaglercraftUltra/.Ultra/Functions/Update.js
@@ -1,8 +1,3 @@
-    <div id="updateDiv2345" style="display: none; width: 500px; height: 100px; background-color: #474747; color: white;; font-size: 10px;">
-      <h1>Your EaglercraftUltra Is Outdated!</h1>
-<br>
-<p>Download The Latest Here: </p>
-    </div>
     <script>
       // Check the local storage for the value of "Client_Version"
       const clientVersion = localStorage.getItem("Client_Version");
