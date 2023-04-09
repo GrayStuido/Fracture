b69ecb693782aaa7f17ca037a45a4ee06e58f621
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2023-04-09 15:59:00 -0700

    Update and rename ClientUpdateCheck.js to ClientUpdateCheckv2.js

--- EaglercraftUltra/.Ultra/Functions/ClientUpdateCheckv2.js
+++ EaglercraftUltra/.Ultra/Functions/ClientUpdateCheckv2.js
@@ -1,11 +1,11 @@
       // Check the local storage for the value of "Client_Version"
       const clientVersion = localStorage.getItem("Client_Version");
       // Compare the value to a set value
-      const setVersion = "2.0.1b";
+      const setVersion = "1.0.0";
       if (clientVersion === setVersion) {
         // If they match, do nothing
       } else {
         // If they don't match, show the existing div on the page
-        const existingDiv = document.getElementById("updateDiv2345");
-        existingDiv.style.display = "flex";
+        const existingDiv = document.getElementById("EaglercraftUltraUpdateDivContainer");
+        existingDiv.style.display = "block";
       }
