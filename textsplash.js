4b3efb54d6c7e2e5ef2a38bae8203893aab1ed6b
Gragin <113940755+GrayStuido@users.noreply.github.com>
2024-02-13 09:59:28 -0800

    Update textsplash.js

--- textsplash.js
+++ textsplash.js
@@ -1,6 +1,18 @@
       const options = [
         "We are so back",
         "I take requests for splash texts",
+        "Down bad for Emulators",
+        "Life Is Roblox - Drake",
+        "Who's in Paris?",
+        "What is your favorite angle?",
+        "The Earth isn't not flat",
+        "Ealgercraft Multiplayer DOESNT WORK IDIOTS",
+        "Please Do Not The Cat",
+        "Canny",
+        "Uncanny",
+        "Making the mother of all omelets here Jack. Can't fret over every egg",
+        "Ultra Rare Splash Text",
+        "Im Not Fixing Doom Lul",
       ];
 
       function changeText() {
