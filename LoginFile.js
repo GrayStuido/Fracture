b36ee09143c87d676cc95f189733b8697e22edfa
Gragin <113940755+GrayStuido@users.noreply.github.com>
2024-06-03 09:29:06 -0700

    Update LoginFile.js

--- LoginFile.js
+++ LoginFile.js
@@ -7,11 +7,11 @@ const users = {
     'Griffon': { password: '07032008GAAP', banned: false, banReason: '', premium: false, profilePicture: 'UserImages/Placeholder.png' }, // Griffon Piazzisi
     'Tysoniscool': { password: '80852612', banned: false, banReason: '', premium: true, profilePicture: 'UserImages/Placeholder.png' }, // Tyson Falhman
     'Oskie': { password: 'La*eggcool456', banned: false, banReason: '', premium: true, profilePicture: 'UserImages/Placeholder.png' }, // Oskie Ayala
-    'Reidak': { password: 'thokbest877!', banned: false, banReason: '', premium: true, profilePicture: 'UserImages/Placeholder.png' }, // Adison
+    'Reidak': { password: 'thokbest877!', banned: false, banReason: '', premium: true, profilePicture: 'UserImages/reidak.webp' }, // Adison
     '9.5kWhiskey': { password: 'SillyGoofy69', banned: false, banReason: '', premium: false, profilePicture: 'UserImages/Placeholder.png' },
     'junior': { password: 'Password2', banned: true, banReason: 'No one likes you', premium: false, profilePicture: 'UserImages/Placeholder.png' }, // Junior Victor
     '@The_realbread24': { password: 'fracture123', banned: true, banReason: '1) Suspected Cheating. 2) Voted ban', premium: false, profilePicture: 'UserImages/Placeholder.png' }, // Iggy
-    'kingalm24': { password: 'W4k3yW4k3y', banned: false, banReason: '', premium: false, profilePicture: 'UserImages/Placeholder.png' }, // Alex Partan
+    'kingalm24': { password: 'W4k3yW4k3y', banned: false, banReason: '', premium: false, profilePicture: 'UserImages/partena.webp' }, // Alex Partan
     'nimrod.': { password: 'ilikefanta', banned: false, banReason: '', premium: false, profilePicture: 'UserImages/Placeholder.png' }, // Westly Porter
     'uhyeah': { password: 'uhyeahh', banned: false, banReason: '', premium: false, profilePicture: 'UserImages/Placeholder.png' },
     'juju_woahmazing': { password: 'Julian200808', banned: false, banReason: '', premium: false, profilePicture: 'UserImages/Placeholder.png' },
