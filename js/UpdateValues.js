4ee726b51e294696168b3bfc030e85d112c40702
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2024-06-21 20:58:38 -0700

    Push 12

--- js/UpdateValues.js
+++ js/UpdateValues.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var amount = 9;
+    var amount = 11;
     var logInFixValue = localStorage.getItem('LogInFix');
 
     if (logInFixValue === null || logInFixValue != amount) {
