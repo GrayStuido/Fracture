3717a52a29f293f1f18c2462e49b28d8d3b02820
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2024-07-02 13:03:04 -0700

    Push 26

--- js/Master.js
+++ js/Master.js
@@ -40,3 +40,27 @@ const asciiArt1 = `
  *****++++++++++++++=====================----------- 
 `;
 console.log(asciiArt1);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scriptUrls = [
+        "js/script1.js",
+        "js/script2.js",
+        "js/script3.js"
+    ];
+
+    function loadScripts(urls) {
+        if (urls.length === 0) return;
+
+        const script = document.createElement("script");
+        script.src = urls[0];
+        script.onload = function() {
+            console.log(`${urls[0]} loaded`);
+            urls.shift(); // Remove the loaded script from the list
+            loadScripts(urls); // Load the next script
+        };
+
+        document.head.appendChild(script);
+    }
+
+    loadScripts(scriptUrls);
+});
