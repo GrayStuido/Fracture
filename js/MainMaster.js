81b378b1c81b7fa85600ee3edba3641e43ee713c
Gragin <113940755+GrayStuido@users.noreply.github.com>
2024-09-13 10:20:19 -0700

    Update MainMaster.js

--- js/MainMaster.js
+++ js/MainMaster.js
@@ -1,13 +1,5 @@
 // auth-check.js
 
-// Check if user is logged in
-document.addEventListener('DOMContentLoaded', () => {
-    const loggedInUser = localStorage.getItem('loggedInUser');
-    if (!loggedInUser) {
-        window.location.href = '../index.html'; // Redirect to login page if not logged in
-    }
-});
-
 const asciiArt1 = `
  ########******************************************* 
 #                                                   *
