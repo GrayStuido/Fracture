50d43232352adf1acf1fbd875b4a7c790a7c029e
Gragin <113940755+GrayStuido@users.noreply.github.com>
2024-09-16 14:29:08 -0700

    Update LoginFile.js

--- LoginFile.js
+++ LoginFile.js
@@ -39,8 +39,8 @@ const users = [
     { username: "Kai", password: "6969420", banned: false, banReason: "", premium: true, profilePicture: "UserImages/Placeholder.png"  }, //
     { username: "Dvid", password: "Davidgame1.", banned: false, banReason: "", premium: true, profilePicture: "UserImages/david.jpg"  }, //
     { username: "", password: "", banned: false, banReason: "", premium: true, profilePicture: "UserImages/Placeholder.png"  }, // David Chorny
-    { username: "Reidak", password: "thokbest877!!", banned: false, banReason: "", premium: true, profilePicture: "UserImages/Placeholder.png"  }, // Adison Burck
-    { username: "Salm", password: "Hundawg", banned: false, banReason: "", premium: true, profilePicture: "UserImages/Placeholder.png"  }, // Sal Aultman-meltz
+    { username: "Reidak", password: "thokbest877!!", banned: false, banReason: "", premium: true, profilePicture: "UserImages/reidak.webp"  }, // Adison Burck
+    { username: "Salm", password: "Hundawg", banned: false, banReason: "", premium: true, profilePicture: "UserImages/The-Company-Drip.gif"  }, // Sal Aultman-meltz
 ];
 
 function login() {
