0afd5209c98c1c83da30cc544d87b77ddc710793
Gragin <113940755+GrayStuido@users.noreply.github.com>
2024-10-10 14:17:40 -0700

    Update AntiTabClose.js

--- js/AntiTabClose.js
+++ js/AntiTabClose.js
@@ -1,11 +1,44 @@
-// Check if the local storage key 'GGBlock' has the value 'installed'
-if (localStorage.getItem('GGBlock') !== 'installed') {
-    // If not, prevent the rest of the script from running
-    throw new Error("GGBlock not installed");
-}
+// page-exit-handler.js
+
+document.addEventListener('click', (event) => {
+    const clickedElement = event.target.closest('a');
+    if (clickedElement) {
+        const isNavGen = clickedElement.id.startsWith('navgen');
+        const linkIndex = isNavGen ? parseInt(clickedElement.id.split('-')[1]) : -1;
+
+        window.navState.lastClicked = {
+            index: linkIndex,
+            time: Date.now(),
+            isNavGen: isNavGen
+        };
+
+        if (isNavGen) {
+            window.navState.links[linkIndex].clicked = true;
+        }
+
+        // Dispatch custom event
+        const navClickEvent = new CustomEvent('navLinkClicked', { 
+            detail: { linkIndex: linkIndex, isNavGen: isNavGen } 
+        });
+        document.dispatchEvent(navClickEvent);
+    }
+});
 
 window.addEventListener('beforeunload', function (e) {
-    var confirmationMessage = 'Are you sure you want to leave this page?';
-    (e || window.event).returnValue = confirmationMessage; // Gecko + IE
-    return confirmationMessage; // Webkit, Safari, Chrome
+    // Check if GGBlock is installed
+    if (localStorage.getItem('GGBlock') !== 'installed') {
+        console.error("GGBlock not installed");
+        return;
+    }
+
+    const lastClicked = window.navState.lastClicked;
+    const recentNavClick = lastClicked && 
+                           (Date.now() - lastClicked.time < 1000) && 
+                           lastClicked.isNavGen;
+
+    if (!recentNavClick) {
+        var confirmationMessage = 'Are you sure you want to leave this page?';
+        (e || window.event).returnValue = confirmationMessage;
+        return confirmationMessage;
+    }
 });
