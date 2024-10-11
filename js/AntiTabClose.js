9e94f36bf81d35daf401863d119823e47e20cd44
Gragin <113940755+GrayStuido@users.noreply.github.com>
2024-10-10 18:11:03 -0700

    Update AntiTabClose.js

--- js/AntiTabClose.js
+++ js/AntiTabClose.js
@@ -1,29 +1,11 @@
-// page-exit-handler.js
+// Check if the local storage key 'GGBlock' has the value 'installed'
+if (localStorage.getItem('GGBlock') !== 'installed') {
+    // If not, prevent the rest of the script from running
+    throw new Error("GGBlock not installed");
+}
 
 window.addEventListener('beforeunload', function (e) {
-    // Check if GGBlock is installed
-    if (localStorage.getItem('GGBlock') !== 'installed') {
-        console.error("GGBlock not installed");
-        return;
-    }
-
-    // Check if Noconfirm is set to 1
-    if (localStorage.getItem('Noconfirm') === '1') {
-        // If Noconfirm is 1, don't show the confirmation dialog
-        return;
-    }
-
-    // If Noconfirm is not 1, show the confirmation dialog
     var confirmationMessage = 'Are you sure you want to leave this page?';
-    (e || window.event).returnValue = confirmationMessage;
-    return confirmationMessage;
-});
-
-// Reset Noconfirm to 0 when a new page starts loading
-window.addEventListener('unload', function() {
-    localStorage.setItem('Noconfirm', '0');
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    localStorage.setItem('Noconfirm', '0');
+    (e || window.event).returnValue = confirmationMessage; // Gecko + IE
+    return confirmationMessage; // Webkit, Safari, Chrome
 });
